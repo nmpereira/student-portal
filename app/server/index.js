a9f327107f616f086e92d5347b00a310f610981c
nmpereira <nikhilpereira25@gmail.com>
2022-01-26 04:08:33 -0500

    connection to mongodb and user api routes

--- app/server/index.js
+++ app/server/index.js
@@ -6,12 +6,21 @@ const logger = require("./routes/logger");
 const api = require("./routes/api");
 const PORT = process.env.PORT || 3001;
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.dbURI);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.error("Connected to db"));
+
 const app = express();
 app.set("json spaces", 2);
 app.use(express.json());
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({
--- app/server/routes/users.js
+++ app/server/routes/users.js
@@ -1,9 +1,20 @@
 "use strict";
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const members = require("./members");
+const uuid = require("uuid");
+const user = require("../models/user");
+
 //Get all Users
-router.route("/").get((req, res) => res.json(members));
+router.route("/").get(async (req, res) => {
+  try {
+    const users = await user.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
 
 //Get single User
 router.route("/:id").get((req, res) => {
@@ -16,6 +27,31 @@ router.route("/:id").get((req, res) => {
       .json({ msg: `No User with the id of '${req.params.id}' found` });
     console.log(`No User with the id of '${req.params.id}' found`);
   }
+});
+
+//Create users with
+router.route("/").post(async (req, res, next) => {
+  const User = new user({
+    // id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: "active",
+  });
+
+  if (!User.name || !User.email) {
+    return res.status(400).json({ msg: "Please include a name and an email" });
+  }
+
+  try {
+    const newUser = await User.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ msg: err.message });
+  }
+
+  // members.push(User);
+  // res.send(["msg: POST Sucess", User]);
+  // next();
 });
 
 module.exports = router;
