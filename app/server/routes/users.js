e7ebe0db686abf28314c6e80ae2f4894424f1689
nmpereira <nikhilpereira25@gmail.com>
2022-01-27 13:28:01 -0500

    add more user routes and db implement

--- app/server/routes/users.js
+++ app/server/routes/users.js
@@ -16,23 +16,22 @@ router.route("/").get(async (req, res) => {
   }
 });
 
-//Get single User
-router.route("/:id").get((req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res
-      .status(400)
-      .json({ msg: `No User with the id of '${req.params.id}' found` });
-    console.log(`No User with the id of '${req.params.id}' found`);
+//Get single User by id
+router.route("/:id").get(async (req, res) => {
+  try {
+    const users = await user.find({ _id: req.params.id });
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
+  // res.json(user);
 });
 
-//Create users with
+//Create users
 router.route("/").post(async (req, res, next) => {
   const User = new user({
     // id: uuid.v4(),
+    username: req.body.username,
     name: req.body.name,
     email: req.body.email,
     status: "active",
@@ -48,10 +47,50 @@ router.route("/").post(async (req, res, next) => {
   } catch (err) {
     res.status(400).json({ msg: err.message });
   }
-
-  // members.push(User);
-  // res.send(["msg: POST Sucess", User]);
-  // next();
 });
+
+//Update single User by id
+router.route("/:id").put(async (req, res) => {
+  const query = { _id: req.params.id };
+
+  const update = {
+    $set: {
+      name: req.body.name,
+      email: req.body.email,
+    },
+  };
+
+  try {
+    const users = await user.findOneAndUpdate(query, update, { new: true });
+    res.send(users);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
+
+//Delete single User by id
+router.route("/:id").get(async (req, res) => {
+  try {
+    const users = await user.find({ _id: req.params.id });
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
+// TODO: implement this middleware function
+// async function getUser(req, res, next) {
+//   let user;
+//   try {
+//     user = await user.find({ _id: req.params.id });
+//     if (user == null) {
+//       return res.status(404).json({ message: "Cannot find user" });
+//     }
+//   } catch (err) {
+//     return res.status(500).json({ message1: err.message });
+//   }
+
+//   res.user = user;
+//   next();
+// }
 
 module.exports = router;
